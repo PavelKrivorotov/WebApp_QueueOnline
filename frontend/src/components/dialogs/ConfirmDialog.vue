18a15c304db05fc0760ea3d4d5532c96ba767c67
PavelKrivorotov <pavel.krivorotov.54@mail.ru>
2023-12-12 18:16:23 +0300

    Add ContentInfoDialog and ConfirmDialog

---
+++ frontend/src/components/dialogs/ConfirmDialog.vue
@@ -0,0 +1,51 @@
+<script setup>
+const props = defineProps({
+    isActive: Boolean,
+    title: {
+        type: String,
+        default: 'Confirm dialog title'
+    },
+    text: {
+        type: String,
+        default: 'Confirmation dialog text'
+    }
+});
+
+const emits = defineEmits([
+    'click:ok',
+    'click:close'
+]);
+</script>
+
+<template>
+    <VDialog
+    v-model="props.isActive"
+    width="400"
+    persistent
+    >
+        <VCard>
+            <VToolbar>
+                <template v-slot:title>
+                    {{ props.title }}
+                </template>
+
+                <template v-slot:append>
+                    <VBtn icon="mdi-close" @click="emits('click:close')"></VBtn>
+                </template>
+            </VToolbar>
+
+            <VCardText>
+                {{ props.text }}
+            </VCardText>
+
+            <VCardActions>
+                <VSpacer></VSpacer>
+                <VBtn @click="emits('click:ok')">Yes</VBtn>
+                <VBtn @click="emits('click:close')">No</VBtn>
+            </VCardActions>
+        </VCard>
+    </VDialog>
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/src/components/dialogs/ContentInfoDialog.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { ref } from 'vue';
+import { useQueueStore } from '../../pinia/queue-store';
+
+const props = defineProps({
+    isActive: Boolean,
+});
+
+const emits = defineEmits([
+    'click:close'
+]);
+
+const queueStore = useQueueStore();
+</script>
+
+<template>
+    <VDialog
+    v-model="props.isActive"
+    width="450"
+    persistent
+    rounded
+    >
+        <VCard>
+            <VToolbar>
+                <template v-slot:title>
+                    Queue information
+                </template>
+
+                <template v-slot:append>
+                    <VBtn icon="mdi-close" @click="emits('click:close')"></VBtn>
+                </template>
+            </VToolbar>
+            
+            <VCardText>
+                <VForm>
+                    <VTextField
+                    label="Title"
+                    v-model="queueStore['queueObject']['title']"
+                    readonly
+                    ></VTextField>
+
+                    <VTextField
+                    label="Key"
+                    v-model="queueStore['queueObject']['key']"
+                    readonly
+                    ></VTextField>
+
+                    <VTextField
+                    label="Date of created"
+                    v-model="queueStore['queueObject']['created']"
+                    readonly
+                    ></VTextField>
+
+                    <VTextField
+                    label="Lifetime"
+                    v-model="queueStore['queueObject']['lifetime']"
+                    readonly
+                    ></VTextField>
+
+                    <VTextField
+                    label="Is active"
+                    v-model="queueStore['queueObject']['state']"
+                    readonly
+                    ></VTextField>
+                </VForm>
+            </VCardText>
+        </VCard>
+    </VDialog>
+</template>
+
+<style style>
+</style>
