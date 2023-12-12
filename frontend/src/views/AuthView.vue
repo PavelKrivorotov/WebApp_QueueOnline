8ef2368b8425171d514f3aca8c22d2caecd77876
PavelKrivorotov <pavel.krivorotov.54@mail.ru>
2023-12-12 20:03:16 +0300

    Add AuthView (page)

---
+++ frontend/src/views/AuthView.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <VToolbar>
+        <template v-slot:append>
+            <VBtn to="/auth/login">Sign In</VBtn>
+            <VBtn to="/auth/registration">Sign Up</VBtn>
+        </template>
+    </VToolbar>
+
+    <RouterView></RouterView>
+</template>
+
+<style scope>
+</style>
