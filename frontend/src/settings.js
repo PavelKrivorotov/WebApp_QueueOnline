b46c9839b15654baa1551b96911e59ba288ba229
PavelKrivorotov <pavel.krivorotov.54@mail.ru>
2023-12-12 18:02:44 +0300

    Add settings.js file

---
+++ frontend/src/settings.js
@@ -0,0 +1,15 @@
+
+// Axios section
+export const AXIOS_BASE_URL = 'http://127.0.0.1:8000';
+export const AXIOS_TIMEOUT = 1000;
+export const AXIOS_HEADERS = {
+    'Content-Type': 'application/json',
+};
+
+
+// Local storage section
+export const LOCALSTORAGE = {
+    EMAIL: 'EMAIL',
+    AUTHORIZATION_TOKEN: 'AUTHORIZATION_TOKEN',
+    QUEUE_KEY: 'QUEUE_KEY',
+};
