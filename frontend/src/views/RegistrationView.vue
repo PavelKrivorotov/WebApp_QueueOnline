55d322ac3f942d663793c9d23f69918355ed4868
PavelKrivorotov <pavel.krivorotov.54@mail.ru>
2023-12-12 18:13:34 +0300

    Add RegistartionView (page)

---
+++ frontend/src/views/RegistrationView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import RegistrationForm from '../components/forms/RegistrationForm.vue'
+</script>
+
+<template>
+    <VContainer fluid>
+        <VRow>
+            <RegistrationForm></RegistrationForm>
+        </VRow>
+    </VContainer>
+</template>
+
+<style>
+</style>
