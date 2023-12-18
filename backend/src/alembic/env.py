b2e87a4f56f9b5ccfe696192394d90a86a41f2ea
PavelKrivorotov <pavel.krivorotov.54@mail.ru>
2023-12-18 13:13:55 +0300

    Alter alembic -> env.py file ... sqlalchemy connection

--- backend/src/alembic/env.py
+++ backend/src/alembic/env.py
@@ -1,11 +1,12 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config, create_engine
 from sqlalchemy import pool
 
 from alembic import context
 
 from database import Base
+from utils import db_url
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -40,7 +41,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    # url = config.get_main_option("sqlalchemy.url")
+    url = db_url();
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -59,10 +61,16 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
+    # connectable = engine_from_config(
+    #     config.get_section(config.config_ini_section, {}),
+    #     prefix="sqlalchemy.",
+    #     poolclass=pool.NullPool,
+    # )
+
+    url = db_url()
+    connectable = create_engine(
+        url = url,
+        poolclass = pool.NullPool
     )
 
     with connectable.connect() as connection:
